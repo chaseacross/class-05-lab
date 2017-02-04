5566a44703bcbb9e3128701f0227ee3c9a7e1e1e
Chase Cross <chaseacross@gmail.com>
2017-02-04 14:19:03 -0800

    Question four functioning 100%

--- app.js
+++ app.js
@@ -56,7 +56,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 testArray = [2,3,4];
-
+function sumArray() {
+  var sumArrayArray = [(testArray[0] + testArray[1] + testArray[2]), testArray + ' was passed in as an array of numbers, and ' + (testArray[0] + testArray[1] + testArray[2]) + ' is their sum.']
+  return sumArrayArray;
+}
 // Here is the test for sumArray(); uncomment it to run it
 testSumArray(testArray);
 
